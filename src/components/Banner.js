9c5d6f5e4234e6b4b7ef482d9f3e349d98178576
Komiljon Yormurodov <ykomiljohn@gmail.com>
2021-09-16 14:58:43 +0500

    setting up

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <>
+      <h1>the.weather</h1>
+      <p>Banner</p>
+    </>
+  )
+}
+
+export default Banner;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <>
+      <h1>Location and Details</h1>
+      <p>Sidebar</p>
+    </>
+  )
+}
+
+export default Sidebar;
